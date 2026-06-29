a173ebeb01ab69a7b22625502abd0fcff13d2e39
wiahweb-lgtm <wiahweb@gmail>
2026-06-29 14:58:03 +0000

    another update

--- javaSc.js
+++ javaSc.js
@@ -1,19 +1,52 @@
-function checkCounties(userCounty){
-  const counties = [
-    "SINOE COUNTY",
-    "MARYLAND COUNTY",
-    "GRAND KRU COUNTY",
-    "RIVER GEE COUNTY",
-    "NIMBA COUNTY"
-  ];
+// function checkCounties(userCounty){
+//   const counties = [
+//     "SINOE COUNTY",
+//     "MARYLAND COUNTY",
+//     "GRAND KRU COUNTY",
+//     "RIVER GEE COUNTY",
+//     "NIMBA COUNTY"
+//   ];
 
-  if(counties.includes(userCounty)){
-    return (userCounty + " is part of the list");
-  }
- else
- {
-  return (userCounty + " does not exist yet ");
- }
-}
+//   if(counties.includes(userCounty)){
+//     return (userCounty + " is part of the list");
+//   }
+//  else
+//  {
+//   return (userCounty + " does not exist yet ");
+//  }
+// }
 
-console.log(checkCounties("NIMBA COUNT"))
+// console.log(checkCounties("NIMBA COUNT"))
+
+// function buyMilk(numberof){
+// var costPrice = numberof * 5.5;
+// console.log(costPrice);
+// }
+// buyMilk(8);
+
+// function buyMilk(money, cost, numberof){
+
+// let balance = Math.floor(cost * numberof / money)
+//   console.log(balance);
+
+//   }
+//   buyMilk(5, 500, 9);
+
+// write a function that tell how many days, weeks, months and years left after you know you age and time to live for.
+
+// function lifeSpan(age){
+
+//   let yearBalance = 120 -age;
+//   let month = yearBalance * 12;
+//   let weeks = yearBalance * 52;
+//   let days = yearBalance * 350;
+//   console.log("year reminding is; " + yearBalance,  " You have " + month, " month to  live for ", " you have " + weeks + " weeks to live for ", "you also have " + days + " days to live for ");
+// }
+// lifeSpan(20);
+
+prompt("What is your full-name Name?")
+prompt("Your score as well?")
+
+let score = 89;
+console.log(score);
+alert(" Your score is " + score);
